0fd9a8314bef603ed60042e3d15d9df88f3381b9
franmurillo94 <franmurillo1994@gmail.com>
2020-09-05 19:45:52 -0300

    colores y queries, diseño de radio buttons y buttons para paint-tools y main-tools (pseudo clases incluidas)

--- TP1/js/main.js
+++ TP1/js/main.js
@@ -1,1 +1,11 @@
 "use strict"
+
+function getRadioValue(name){
+    for (var i = 0; i < document.getElementsByName(name).length; i++){
+        if (document.getElementsByName(name)[i].checked){
+            return document.getElementsByName(name)[i].value;
+        }
+    }
+}
+
+let paint_tool = getRadioValue('paint-tool');

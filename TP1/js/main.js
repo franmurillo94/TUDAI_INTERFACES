98f08f47662fad75f5aeda196bdb322672e7bb82
franmurillo94 <franmurillo1994@gmail.com>
2020-09-08 17:47:49 -0300

    tp1| altura de div de tools, cargar imagen de disco e iniciar canvas en blanco

--- TP1/js/main.js
+++ TP1/js/main.js
@@ -1,5 +1,6 @@
 "use strict"
 
+// funcion valores radio button
 function getRadioValue(name){
     for (var i = 0; i < document.getElementsByName(name).length; i++){
         if (document.getElementsByName(name)[i].checked){
@@ -7,9 +8,65 @@ function getRadioValue(name){
         }
     }
 }
-
+// obtiene valores de los radio paint-tool
 let paint_tool = getRadioValue('paint-tool');
-
+// muestra y oculta barra de herramientas 'filtros'
 let filter_tools = document.querySelector('.filter-btn').addEventListener('click',function() {
     document.getElementById('filter-toggle').classList.toggle('active')
 });
+// canvas y contexto
+let canvas = document.querySelector('.canvas');
+let ctx = canvas.getContext('2d');
+// canvas color blanco
+ctx.fillStyle = 'rgba(255,255,255,1)';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+let input = document.querySelector('.file');
+
+input.onchange = e => {
+
+    // getting a hold of the file reference
+    let file = e.target.files[0];
+
+    // setting up the reader
+    let reader = new FileReader();
+    reader.readAsDataURL(file); // this is reading as data url
+
+    // here we tell the reader what to do when it's done reading...
+    reader.onload = readerEvent => {
+        let content = readerEvent.target.result; // this is the content!
+
+        let image = new Image();
+        //image.crossOrigin = 'Anonymous';
+
+        image.src = content;
+
+        image.onload = function () {
+            let imageAspectRatio = (1.0 * this.height) / this.width;
+            let imageScaledWidth = canvas.width;
+            let imageScaledHeight = canvas.width * imageAspectRatio;
+
+            // draw image on canvas
+            ctx.drawImage(this, 0, 0, imageScaledWidth, imageScaledHeight);
+
+            // get imageData from content of canvas
+            let imageData = ctx.getImageData(0, 0, imageScaledWidth, imageScaledHeight);
+
+            // modify imageData
+            for (let j = 0; j < imageData.height; j++) {
+                for (let i = 0; i < imageData.width; i++) {
+                    if (i % 2 == 0) {
+                        let index = (i + imageData.width * j) * 4;
+                        imageData.data[index + 0] = 0;
+                        imageData.data[index + 1] = 0;
+                        imageData.data[index + 2] = 0;
+                    }
+                }
+            }
+            
+            // draw the modified image
+            ctx.putImageData(imageData, 0, 0);
+        }
+    }
+}
+

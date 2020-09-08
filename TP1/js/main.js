f8abb708900405cbfa496b61b08955028277e2f2
franmurillo94 <franmurillo1994@gmail.com>
2020-09-08 12:34:58 -0300

    tp1| barra desplegable (con display none)- provisoria

--- TP1/js/main.js
+++ TP1/js/main.js
@@ -9,3 +9,7 @@ function getRadioValue(name){
 }
 
 let paint_tool = getRadioValue('paint-tool');
+
+let filter_tools = document.querySelector('.filter-btn').addEventListener('click',function() {
+    document.getElementById('filter-toggle').classList.toggle('active')
+});
